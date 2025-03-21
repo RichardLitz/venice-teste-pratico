bacd1e57de1e35e21db20c3a71a1152b5b612e10
Richard Litz <richardlitz@gmail.com>
2025-03-21 16:28:24 -0300

    criando as migrations

---
+++ database/migrations/2025_03_21_192643_create_produtos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->integer('categoria_id');
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+            $table->integer('marca_id');
+            $table->foreign('marca_id')->references('id')->on('marcas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
